c462ebadfadbf23ab033af1a353e9f81b62f6427
Frank Mohaupt <frank.mohaupt@brainworx.audio>
2019-10-04 03:11:49 +0200

    reorder

--- MoneyEarned.lua
+++ MoneyEarned.lua
@@ -4,7 +4,7 @@ local _, ns = ...;
 ns.blnDebug = false;
 ns.blnPrinted = false;
 ns.intMyMoney = 0;
-ns.strChatFrame = "ChatFrame1";
+ns.strChatFrame = "";
 ns.strName = "|cff628ea5MoneyEarned|r";
 ns.tblFrame = CreateFrame("Frame");
 
@@ -20,28 +20,6 @@ function ns.debug(strDebugMessage)
 end
 
 --[[
--- Set Slash commands that can be used in-game to interact with the addon
-]]
-function ns.setSlashCommands()
-	ns.debug("setSlashCommands()");
-	SLASH_MONEYEARNED1 = "/me"
-	SlashCmdList["MONEYEARNED"] = function(msg, editBox)
-		ns:slashHandler(msg, editBox);
-	end
-end
-
---[[
--- Handles the slash commands
-]]
-function ns:slashHandler(msg, editBox)
-	ns.debug("slashHandler()");
-	if string.find(msg, "debug") then
-		ns.blnDebug = not ns.blnDebug;
-		print("Debugging " .. (ns.blnDebug and "ON" or "OFF"));
-	end
-end
-
---[[
 -- Calls different functions on different events
 ]]
 function ns.frameOnEventScript(self, strEvent)
@@ -53,6 +31,22 @@ function ns.frameOnEventScript(self, strEvent)
 	elseif string.find(strEvent, "_CLOSED") then
 		ns.onClosed();
 	end
+end
+
+--[[
+-- Get the Chat Frame or default Chat Frame
+]]
+function ns.getChatFrame()
+	ns.debug("getChatFrame()");
+	return ns.strChatFrame or DEFAULT_CHAT_FRAME;
+end
+
+--[[
+-- Get the Events or empty table
+]]
+function ns.getEvents()
+	ns.debug("getEvents()");
+	return ns.tblEvents or {};
 end
 
 --[[
@@ -78,6 +72,17 @@ function ns.onClosed()
 end
 
 --[[
+-- Send Message to Chat Frame
+]]
+function ns.printToChatFrame(strMessage)
+	ns.debug("printToChatFrame()");
+	ns.debug("strMessage: " .. strMessage);
+
+	ns.debug("Print to " .. ns.getChatFrame());
+	_G[ns.getChatFrame()]:AddMessage(strMessage);
+end
+
+--[[
 -- Register the events
 ]]
 function ns.registerEvents()
@@ -87,28 +92,6 @@ function ns.registerEvents()
 			ns.tblFrame:RegisterEvent(strEvent);
 			ns.debug(strEvent);
 	end
-end
-
---[[
--- Set the Events
-]]
-function ns.setEvents()
-	ns.debug("setEvents()");
-	ns.tblEvents = {};
-	
-	for _, strSubject in pairs({"AUCTION_HOUSE", "MAIL", "MERCHANT", "TRADE", "TRAINER"}) do
-		for _, strAction in pairs({"_SHOW", "_CLOSED"}) do
-			table.insert(ns.tblEvents, strSubject .. strAction);
-		end
-	end
-end
-
---[[
--- Get the Events or empty table
-]]
-function ns.getEvents()
-	ns.debug("getEvents()");
-	return ns.tblEvents or {};
 end
 
 --[[
@@ -135,22 +118,39 @@ function ns.setChatFrame(strName)
 end
 
 --[[
--- Get the Chat Frame or default Chat Frame
+-- Set the Events
 ]]
-function ns.getChatFrame()
-	ns.debug("getChatFrame()");
-	return ns.strChatFrame or "ChatFrame1";
+function ns.setEvents()
+	ns.debug("setEvents()");
+	ns.tblEvents = {};
+	
+	for _, strSubject in pairs({"AUCTION_HOUSE", "MAIL", "MERCHANT", "TRADE", "TRAINER"}) do
+		for _, strAction in pairs({"_SHOW", "_CLOSED"}) do
+			table.insert(ns.tblEvents, strSubject .. strAction);
+		end
+	end
 end
 
 --[[
--- Send Message to Chat Frame
+-- Set Slash commands that can be used in-game to interact with the addon
 ]]
-function ns.printToChatFrame(strMessage)
-	ns.debug("printToChatFrame()");
-	ns.debug("strMessage: " .. strMessage);
+function ns.setSlashCommands()
+	ns.debug("setSlashCommands()");
+	SLASH_MONEYEARNED1 = "/me"
+	SlashCmdList["MONEYEARNED"] = function(msg, editBox)
+		ns:slashHandler(msg, editBox);
+	end
+end
 
-	ns.debug("Print to " .. ns.getChatFrame());
-	_G[ns.getChatFrame()]:AddMessage(strMessage);
+--[[
+-- Handles the slash commands
+]]
+function ns:slashHandler(msg, editBox)
+	ns.debug("slashHandler()");
+	if string.find(msg, "debug") then
+		ns.blnDebug = not ns.blnDebug;
+		print("Debugging " .. (ns.blnDebug and "ON" or "OFF"));
+	end
 end
 
 --[[
