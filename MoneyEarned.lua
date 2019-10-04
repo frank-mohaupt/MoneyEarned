78f54871848c575d011ddadef9fb34f2ab0fac53
Frank Mohaupt <frank.mohaupt@brainworx.audio>
2019-10-04 03:02:24 +0200

    comments added to functions
    reordered
    setter and getter for events and chat frames

--- MoneyEarned.lua
+++ MoneyEarned.lua
@@ -1,12 +1,17 @@
 local _, ns = ...;
 
-ns.strName = "|cff628ea5MoneyEarned|r";
 
 ns.blnDebug = false;
-ns.tblFrame = CreateFrame("Frame");
-ns.intMyMoney = 0;
 ns.blnPrinted = false;
+ns.intMyMoney = 0;
+ns.strChatFrame = "ChatFrame1";
+ns.strName = "|cff628ea5MoneyEarned|r";
+ns.tblFrame = CreateFrame("Frame");
 
+--[[
+-- Can be used to print out debugging information
+-- Switch ON/OFF with "-debug"
+]]
 function ns.debug(strDebugMessage)
 	local strDebugPrefix = ns.strName .. " DEBUG: ";
 	if ns.blnDebug then
@@ -14,74 +19,153 @@ function ns.debug(strDebugMessage)
 	end
 end
 
-function ns:SlashHandler(msg, editBox)
-	if string.find(msg, "-debug") then
+--[[
+-- Set Slash commands that can be used in-game to interact with the addon
+]]
+function ns.setSlashCommands()
+	ns.debug("setSlashCommands()");
+	SLASH_MONEYEARNED1 = "/me"
+	SlashCmdList["MONEYEARNED"] = function(msg, editBox)
+		ns:slashHandler(msg, editBox);
+	end
+end
+
+--[[
+-- Handles the slash commands
+]]
+function ns:slashHandler(msg, editBox)
+	ns.debug("slashHandler()");
+	if string.find(msg, "debug") then
 		ns.blnDebug = not ns.blnDebug;
 		print("Debugging " .. (ns.blnDebug and "ON" or "OFF"));
 	end
 end
 
+--[[
+-- Calls different functions on different events
+]]
 function ns.frameOnEventScript(self, strEvent)
 	ns.debug("frameOnEventScript()");
 	ns.debug("strEvent: " .. strEvent);
-	ns.intMoneyEarned = 0;
+
 	if string.find(strEvent, "_SHOW") then
-		ns.blnPrinted = false;
-		ns.intMyMoney = GetMoney();
-	elseif string.find(strEvent, "_CLOSED") and ns.blnPrinted == false then
-		ns.intMoneyEarned = GetMoney() - ns.intMyMoney;
-		ns.strSign = ns.intMoneyEarned < 0 and "-" or "+";
-		if ns.intMoneyEarned ~= 0 then
-			ns.printToChatFrame(ns.strSign .. GetCoinTextureString(math.abs(ns.intMoneyEarned)), "Money");
-		end
+		ns.onShow();
+	elseif string.find(strEvent, "_CLOSED") then
+		ns.onClosed();
+	end
+end
+
+--[[
+-- on show
+]]
+function ns.onShow()
+	ns.debug("onShow()");
+	ns.blnPrinted = false;
+	ns.intMyMoney = GetMoney();
+end
+
+--[[
+-- on closed
+]]
+function ns.onClosed()
+	ns.debug("onClosed()");
+	ns.intMoneyEarned = GetMoney() - ns.intMyMoney;
+	ns.strSign = ns.intMoneyEarned < 0 and "-" or "+";
+	if ns.intMoneyEarned ~= 0 and ns.blnPrinted == false then
+		ns.printToChatFrame(ns.strSign .. GetCoinTextureString(math.abs(ns.intMoneyEarned)), "Money");
 		ns.blnPrinted = true;
 	end
-	ns.debug("Money on " .. strEvent .. ": " .. GetCoinTextureString(GetMoney()));
 end
 
+--[[
+-- Register the events
+]]
 function ns.registerEvents()
 	ns.debug("registerEvents()");
-	for _, strEventPrefix in pairs({"AUCTION_HOUSE", "MAIL", "MERCHANT", "TRADE", "TRAINER"}) do
-		for _, strEventSuffix in pairs({"_SHOW", "_CLOSED"}) do
-			local strEventName = strEventPrefix .. strEventSuffix;
-			ns.tblFrame:RegisterEvent(strEventName);
-			ns.debug(strEventName);
+	local tblEvents = ns.getEvents();
+	for _, strEvent in pairs(tblEvents) do
+			ns.tblFrame:RegisterEvent(strEvent);
+			ns.debug(strEvent);
+	end
+end
+
+--[[
+-- Set the Events
+]]
+function ns.setEvents()
+	ns.debug("setEvents()");
+	ns.tblEvents = {};
+	
+	for _, strSubject in pairs({"AUCTION_HOUSE", "MAIL", "MERCHANT", "TRADE", "TRAINER"}) do
+		for _, strAction in pairs({"_SHOW", "_CLOSED"}) do
+			table.insert(ns.tblEvents, strSubject .. strAction);
 		end
 	end
 end
 
-function ns.printToChatFrame(strMessage, strName)
-	ns.debug("printToChatFrame()");
-	ns.debug("strMessage: " .. strMessage);
+--[[
+-- Get the Events or empty table
+]]
+function ns.getEvents()
+	ns.debug("getEvents()");
+	return ns.tblEvents or {};
+end
+
+--[[
+-- Set the Chat Frame
+]]
+function ns.setChatFrame(strName)
+	ns.debug("setChatFrame()");
 	ns.debug("strName: " .. strName);
-	
 	local intChatTab = 1;
 	local strChatFrameName = "";
 	
-	for i = 1, NUM_CHAT_WINDOWS do
-		strChatFrameName = GetChatWindowInfo(i);
+	for intTab = 1, NUM_CHAT_WINDOWS do
+		strChatFrameName = GetChatWindowInfo(intTab);
 		
 		ns.debug("ChatFrameName: " .. strChatFrameName);
 		
 		if strChatFrameName == strName then
-			ns.debug("Set intChatTab to :" .. i);
-			intChatTab = i;
+			intChatTab = intTab;
 			break;
 		end
 	end
-	
-	ns.debug("Print to ChatFrame" .. intChatTab);
-	_G["ChatFrame".. intChatTab]:AddMessage(strMessage);
+	ns.strChatFrame = "ChatFrame" .. intChatTab;
+	ns.debug("Set ChatFrame to " .. ns.strChatFrame);
 end
 
+--[[
+-- Get the Chat Frame or default Chat Frame
+]]
+function ns.getChatFrame()
+	ns.debug("getChatFrame()");
+	return ns.strChatFrame or "ChatFrame1";
+end
+
+--[[
+-- Send Message to Chat Frame
+]]
+function ns.printToChatFrame(strMessage)
+	ns.debug("printToChatFrame()");
+	ns.debug("strMessage: " .. strMessage);
+
+	ns.debug("Print to " .. ns.getChatFrame());
+	_G[ns.getChatFrame()]:AddMessage(strMessage);
+end
+
+--[[
+-- Sets different initial values
+]]
 function ns.init()
-	ns.printToChatFrame(ns.strName .. " loaded!", "Money");
-	ns.tblFrame:SetScript("OnEvent", ns.frameOnEventScript);
+	ns.debug("init()");
+	ns.setChatFrame("Money");
+	ns.setEvents();
+	ns.setSlashCommands();
 	ns.registerEvents();
-	SLASH_MONEYEARNED1 = "/me"
-	SlashCmdList["MONEYEARNED"] = function(msg, editBox)
-		ns:SlashHandler(msg, editBox);
-	end
+
+	ns.tblFrame:SetScript("OnEvent", ns.frameOnEventScript);
+	
+	ns.printToChatFrame(ns.strName .. " loaded!");
 end
 
 ns.init();
